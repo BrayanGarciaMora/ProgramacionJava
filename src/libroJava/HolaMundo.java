a6bee6c2a03db8921a318bddc868815987a5c7fa
BrayanGarciaMora <1brayan2garcia@gmail.com>
2021-01-29 21:55:16 -0400

    Modificancion del codigo holamundo

--- src/libroJava/HolaMundo.java
+++ src/libroJava/HolaMundo.java
@@ -4,7 +4,26 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hola Mundo!!");
+		System.out.println("Hola Mundo!!");
+		operaciones multiplicacion = new operaciones();
+		System.out.println(multiplicacion.devolverResultado());
+		
 	}
 
+}
+
+class operaciones {
+	int b;
+	int r;
+	int a;
+
+	operaciones() {
+		a = 4;
+		b = 5;
+
+	}
+	
+	public int devolverResultado() {
+		return r = a *b;
+	}
 }
